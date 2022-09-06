0b45a09639cce879424bf4f4340ee1aaed56c573
dima-tolmachev <dima.tolmachev@outlook.com>
2022-09-07 00:53:28 +0300

    routing logic removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import './App.scss';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Portfolio from './pages/Portfolio';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Portfolio />} />
-        <Route path="*" element={<Portfolio />} />
-      </Routes>
-    </BrowserRouter>
+    <Portfolio />
   );
 };
 
