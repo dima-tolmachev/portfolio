9f96ca256ebc1cf4261ac65fcced7de8e33b1008
dima-tolmachev <dima.tolmachev@outlook.com>
2022-09-09 00:28:04 +0300

    filter: adaptibility add, label add, counter remove

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,18 +1,12 @@
 import { ChangeEvent } from 'react';
-import { projectObject } from '../../@types/project';
 import { appStore } from '../../stores/app-store';
 import './style.scss';
 
 const Filter = () => {
-  const projectsList = appStore.projectsList;
-  const allProjectsAmount = projectsList.length;
-  const commercialAmount = projectsList.filter(
-    (el: projectObject) => el.type === 3,
-  ).length;
-  const uncommercialAmount = allProjectsAmount - commercialAmount;
-
   return (
     <div className="filter">
+      <h2>Here are some of my projects</h2>
+      
       <select
         onChange={(e: ChangeEvent<HTMLSelectElement>) => {
           appStore.order = parseInt(e.target.value);
@@ -20,9 +14,9 @@ const Filter = () => {
         name="filter"
         id="filter"
       >
-        <option value="1">All projects - {allProjectsAmount}</option>
-        <option value="2">Commercial - {commercialAmount}</option>
-        <option value="3">For fun - {uncommercialAmount}</option>
+        <option value="1">All</option>
+        <option value="2">Commercial</option>
+        <option value="3">For fun</option>
       </select>
     </div>
   );
